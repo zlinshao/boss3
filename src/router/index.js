d8cc7bea4976ea07dbe9106b980e8076c4d2c6b2
lin60126 <601267015@qq.com>
2018-01-30 20:55:45 +0800

    ach

--- src/router/index.js
+++ src/router/index.js
@@ -32,6 +32,11 @@ import StaticDetail from '../components/finance/statistics/components/staticDeta
 import EndPayment from '../components/finance/endPayment/index'                       //尾款报备
 import IncomeFlow from '../components/finance/incomeFlow/index'                       //收支流水
 
+// 业绩工资
+import PeriodicTable from '../components/meritPay/periodicTable/index'
+import Achievement from '../components/meritPay/achievement/index'
+
+
 //租赁管理
 import WholeRentManage from '../components/rentManage/wholeRentManage/index.vue'
 import JointRentManage from '../components/rentManage/jointRentManage/index.vue'
@@ -58,8 +63,8 @@ import CapitalGather from '../components/OAWork/property/items/capitalAssetsGath
 
 //人资管理
 import StaffManage from '../components/humanResource/staffManage/index.vue'
-import Organization from '../components/humanResource/organization/index.vue'
-import Achievement from '../components/humanResource/achievement/index.vue'
+// import Organization from '../components/humanResource/organization/index.vue'
+// import Achievement from '../components/humanResource/achievement/index.vue'
 
 import RentingDetail from '../components/rentManage/rentingDetail.vue'
 
@@ -91,25 +96,25 @@ export default new Router({
     {
       path: '/',
       component: Index,
-      hidden:true,
+      hidden: true,
       name: 'Index',
       children: [
-        { path: '/main', component: Main, name: '主页',icon:'el-icon-menu'},
-        { path: '/commonComponent', component: CommonComponent, name: '通用组件',icon:'el-icon-goods'},
+        {path: '/main', component: Main, name: '主页', icon: 'el-icon-menu'},
+        {path: '/commonComponent', component: CommonComponent, name: '通用组件', icon: 'el-icon-goods'},
       ]
     },
     {
       path: '/',
       component: Index,
       name: '租赁管理',
-      icon:'el-icons-fa-home',
+      icon: 'el-icons-fa-home',
       children: [
-        { path: '/wholeRentManage', component: WholeRentManage, name: '整租管理',},
-        { path: '/jointRentManage', component: JointRentManage, name: '合租管理',},
-        { path: '/housesManage', component: HousesManage, name: '房屋管理',},
-        { path: '/clientManage', component: ClientManage, name: '客户管理',},
-        { path: '/contractManage', component: ContractManage, name: '合同管理',},
-        { path: '/repairManage', component: RepairManage, name: '维修管理',}
+        {path: '/wholeRentManage', component: WholeRentManage, name: '整租管理',},
+        {path: '/jointRentManage', component: JointRentManage, name: '合租管理',},
+        {path: '/housesManage', component: HousesManage, name: '房屋管理',},
+        {path: '/clientManage', component: ClientManage, name: '客户管理',},
+        {path: '/contractManage', component: ContractManage, name: '合同管理',},
+        {path: '/repairManage', component: RepairManage, name: '维修管理',}
       ]
     },
 
@@ -117,16 +122,16 @@ export default new Router({
       path: '/',
       component: Index,
       name: '财务账本',
-      icon:'el-icon-tickets',
+      icon: 'el-icon-tickets',
       children: [
-        { path: '/accountManage', component: AccountManage, name: '账户管理',},
-        { path: '/subjectManage', component: SubjectManage, name: '科目管理',},
-        { path: '/roomCharge', component: RoomCharge, name: '房租款项总汇',},
-        { path: '/otherSum', component: OtherSum, name: '其余款项总汇',},
-        { path: '/statistics', component: Statistics, name: '统计',},
-        { path: '/budgetManage', component: BudgetManage, name: '预算管理',},
-        { path: '/endPayment', component: EndPayment, name: '尾款报备',},
-        { path: '/incomeFlow', component: IncomeFlow, name: '收支流水',},
+        {path: '/accountManage', component: AccountManage, name: '账户管理',},
+        {path: '/subjectManage', component: SubjectManage, name: '科目管理',},
+        {path: '/roomCharge', component: RoomCharge, name: '房租款项总汇',},
+        {path: '/otherSum', component: OtherSum, name: '其余款项总汇',},
+        {path: '/statistics', component: Statistics, name: '统计',},
+        {path: '/budgetManage', component: BudgetManage, name: '预算管理',},
+        {path: '/endPayment', component: EndPayment, name: '尾款报备',},
+        {path: '/incomeFlow', component: IncomeFlow, name: '收支流水',},
       ]
     },
     {
@@ -136,74 +141,88 @@ export default new Router({
       hidden: true
     },
 
+    {
+      path: '/',
+      component: Index,
+      name: '业绩工资',
+      icon: 'el-icons-fa-money',
+      children: [
+        {path: '/periodicTable', component: PeriodicTable, name: '周期表',},
+        {path: '/achievement', component: Achievement, name: '业绩',},
+      ]
+    },
 
     {
       path: '/',
       component: Index,
       name: '人资管理',
-      icon:'el-icons-fa-sitemap',
+      icon: 'el-icons-fa-sitemap',
       children: [
-        { path: '/staffManage', component: StaffManage, name: '用户管理',},
-        { path: '/organization', component: Organization, name: '组织架构',},
-        { path: '/achievement', component: Achievement, name: '业绩工资',},
+        {path: '/staffManage', component: StaffManage, name: '用户管理',},
+        // {path: '/organization', component: Organization, name: '组织架构',},
+        // {path: '/achievement', component: Achievement, name: '业绩工资',},
       ]
     },
     {
       path: '/',
       component: Index,
       name: 'OA办公',
-      icon:'el-icons-fa-clipboard',
+      icon: 'el-icons-fa-clipboard',
       children: [
-        { path: '/property', component: Property, icon:'el-icons-fa-money',name: '资产管理',
-          children:[
-            { path: '/property/suppliesUse',component: SuppliesUse, name: '办公用品申领'},
-            { path: '/property/suppliesGather',component: SuppliesGather, name: '办公用品汇总'},
-            { path: '/property/capitalChange',component: CapitalChange, name: '固定资产异动'},
-            { path: '/property/capitalGather',component: CapitalGather, name: '固定资产汇总'},
+        {
+          path: '/property', component: Property, icon: 'el-icons-fa-money', name: '资产管理',
+          children: [
+            {path: '/property/suppliesUse', component: SuppliesUse, name: '办公用品申领'},
+            {path: '/property/suppliesGather', component: SuppliesGather, name: '办公用品汇总'},
+            {path: '/property/capitalChange', component: CapitalChange, name: '固定资产异动'},
+            {path: '/property/capitalGather', component: CapitalGather, name: '固定资产汇总'},
           ]
         },
 
-        { path: '/communication', component: Communication, name: '通讯管理',},
-        { path: '/programme', component: Programme, name: '日程管理',},
-        { path: '/knowledge', component: Knowledge, name: '知识管理',},
-        { path: '/institution', component: Institution, name: '制度管理',},
-        { path: '/portals', component: Portals, name: '门户管理',},
-        { path: '/vote', component: Vote, name: '投票管理',},
-        { path: '/contractClaim', component: ContractClaim, name: '合同认领',},
+        {path: '/communication', component: Communication, name: '通讯管理',},
+        {path: '/programme', component: Programme, name: '日程管理',},
+        {path: '/knowledge', component: Knowledge, name: '知识管理',},
+        {path: '/institution', component: Institution, name: '制度管理',},
+        {path: '/portals', component: Portals, name: '门户管理',},
+        {path: '/vote', component: Vote, name: '投票管理',},
+        {path: '/contractClaim', component: ContractClaim, name: '合同认领',},
       ]
     },
     {
       path: '/',
       component: Index,
       name: '系统设置',
-      icon:'el-icon-setting',
+      icon: 'el-icon-setting',
       children: [
 
-        { path: '/recycle',
+        {
+          path: '/recycle',
           component: Recycle,
-          icon:'el-icon-delete',
+          icon: 'el-icon-delete',
           name: '回收站',
         },
-        { path: '/contractMould',
+        {
+          path: '/contractMould',
           component: ContractMould,
-          icon:'el-icon-document',
+          icon: 'el-icon-document',
           name: '合同模板',
         },
-        { path: '/message',
+        {
+          path: '/message',
           component: Message,
-          icon:'el-icon-message',
+          icon: 'el-icon-message',
           name: '短信模块',
-          children:[
-            { path: '/message/messageList',component: MessageList, name: '短信列表'},
-            { path: '/message/messageModule',component: MessageModule, name: '短信模板'},
+          children: [
+            {path: '/message/messageList', component: MessageList, name: '短信列表'},
+            {path: '/message/messageModule', component: MessageModule, name: '短信模板'},
           ]
         },
-        { path: '/loginRecord', component: LoginRecord,name: '登陆日志',},
-        { path: '/userDictionary', component: UserDictionary, name: '用户字典',},
-        { path: '/roleManage', component: RoleManage, name: '角色管理',},
-        { path: '/powerManage', component: PowerManage, name: '权限管理',},
-        { path: '/villageManage', component: VillageManage, name: '小区管理',},
-        { path: '/exportManage', component: ExportManage, name: '导出管理',},
+        {path: '/loginRecord', component: LoginRecord, name: '登陆日志',},
+        {path: '/userDictionary', component: UserDictionary, name: '用户字典',},
+        {path: '/roleManage', component: RoleManage, name: '角色管理',},
+        {path: '/powerManage', component: PowerManage, name: '权限管理',},
+        {path: '/villageManage', component: VillageManage, name: '小区管理',},
+        {path: '/exportManage', component: ExportManage, name: '导出管理',},
       ]
     },
 
