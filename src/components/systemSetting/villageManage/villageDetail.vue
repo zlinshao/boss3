1031912ef991ab68985b33629087803eef43cc59
lin60126 <601267015@qq.com>
2018-02-28 16:42:06 +0800

    village

---
+++ src/components/systemSetting/villageManage/villageDetail.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "village-detail",
+        data() {
+            return {}
+        },
+        mounted() {
+        },
+        watch: {},
+        methods: {},
+    }
+</script>
+
+<style scoped>
+
+</style>
