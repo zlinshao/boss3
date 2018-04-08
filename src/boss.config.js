90de5d37c385bb7f7c844e9e97c4d98af952dd88
lin60126 <601267015@qq.com>
2018-04-08 22:40:15 +0800

    fdsfsda

--- src/boss.config.js
+++ src/boss.config.js
@@ -5,6 +5,8 @@ globalConfig = {
   server_user :'http://test.boss-support.lejias.cn/api/s1/',          //人资模块接口地址
   server_token:"http://test.boss-support.lejias.cn/",                 //认证接口
   env: 'dev',
+  client_secret: 'udMntGnEJBgsevojFrMicLuW8G2ABBAsmRlK9fIC',
+  client_id: '2',
   header:{
     //'Content-Type': 'application/x-www-form-urlencoded',
     'Accept': 'application/json',
