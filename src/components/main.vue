c346d570f8d7c8d20b6659130853b7f5fbef8332
cj_jing <1429407967@qq.com>
2018-04-09 21:07:23 +0800

    'table'

--- src/components/main.vue
+++ src/components/main.vue
@@ -625,11 +625,11 @@
     overflow:hidden;
     text-overflow:ellipsis;
     display:-webkit-box;
-    -webkit-box-orient:vertical;
     -webkit-line-clamp:2;
     font-size: 14px;
     line-height: 20px;
     -webkit-box-orient: vertical;
+    box-orient: vertical;
     word-break: break-all;
   }
   .t_center{
