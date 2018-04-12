1194ba4c658caade47a3bbadd4f5f001eefac075
cj_jing <1429407967@qq.com>
2018-04-12 20:08:27 +0800

    'organtree'

--- src/components/systemSetting/moduleSetting/packageManagement/index.vue
+++ src/components/systemSetting/moduleSetting/packageManagement/index.vue
@@ -1,10 +1,10 @@
 <template>
-  <div>
-    <el-form size="mini" >
+  <div class="main">
+    <el-form size="small"  label-width="200px">
       <el-row>
         <el-form-item v-for="item in 5" :key="item.id">
           <el-col :span="2" style="color:#409EFF;">业绩包</el-col>
-          <el-col :span="5">
+          <el-col :span="10">
             <el-input placeholder="请选择部门" v-model="department"  @focus="selectDepart">
             <template slot="append">
               <div style="cursor: pointer;" @click="emptyDepart">清空</div>
@@ -14,10 +14,14 @@
         </el-form-item>
       </el-row>
     </el-form>
-    <span slot="footer" class="dialog-footer">
-      <span>当前剩余 xxx 部门未设置业绩包</span><span style="color:#409EFF;cursor: pointer" @click="viewDetail">查看详情</span>
-      <el-button size="small" type="primary" @click="">确 定</el-button>
+
+    <span style="color:#fdaece;margin-left:400px;">
+      <span>备注：当前剩余 xxx 部门未设置业绩包</span>
+      <el-button size="mini" @click="viewDetail" round> 查看详情 </el-button>
     </span>
+    <div style="margin-top: 120px;margin-left: 400px;">
+      <el-button @click=""  type="primary" size="small" style="padding: 10px 140px;">保存</el-button>
+    </div>
     <el-dialog title="未设置套餐部门" :visible.sync="noSetMealDepartVisible" width="30%">
 
       <el-button size="small" type="primary" @click="noSetMealDepartVisible=false">确 定</el-button>
@@ -67,5 +71,7 @@
 </script>
 
 <style scoped>
-
+  .main {
+    padding: 40px 120px;
+  }
 </style>
