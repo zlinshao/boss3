6e39e43ec0ef6581221f7adb6ba8933b07a303f3
aigle <840207473@qq.com>
2018-04-21 16:50:03 +0800

    ddd

--- src/components/rentManage/housesManage/components/houseDetail.vue
+++ src/components/rentManage/housesManage/components/houseDetail.vue
@@ -60,6 +60,7 @@
                   </el-form-item>
                 </el-col>
               </el-row>
+
               <el-row>
                 <el-col :span="8">
                   <el-form-item label="房屋类型">
