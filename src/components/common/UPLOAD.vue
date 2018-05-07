8510701be54d77eb9261aaedb3aae7592293d92d
aigle <840207473@qq.com>
2018-05-07 15:54:54 +0800

    ddd

--- src/components/common/UPLOAD.vue
+++ src/components/common/UPLOAD.vue
@@ -186,13 +186,10 @@
 
             'BeforeUpload': function (up, file) {
               // 每个文件上传前，处理相关的事情
-
-
             },
             'UploadProgress': function (up, file) {
               // 每个文件上传时，处理相关的事情
               if (document.getElementById(file.id)) {
-
                 if (file.percent < 100) {
                   document.getElementById(file.id).getElementsByTagName('p')[0].innerHTML = `
                   <div role="progressbar" aria-valuenow="10" aria-valuemin="${file.percent}" aria-valuemax="100" class="el-progress el-progress--circle"><div class="el-progress-circle" style="height: 80px; width: 80px;background: #fff;opacity:.7;border-radius: 50%;"><svg viewBox="0 0 100 100"><path d="M 50 50 m 0 -47 a 47 47 0 1 1 0 94 a 47 47 0 1 1 0 -94" stroke="#e5e9f2" stroke-width="4.8" fill="none" class="el-progress-circle__track"></path><path d="M 50 50 m 0 -47 a 47 47 0 1 1 0 94 a 47 47 0 1 1 0 -94" stroke-linecap="round" stroke="#20a0ff" stroke-width="4.8" fill="none" class="el-progress-circle__path" style="stroke-dasharray: 299.08px, 299.08px; stroke-dashoffset: ${299.08 - (299.08/100)*file.percent}px; transition: stroke-dashoffset 0.6s ease 0s, stroke 0.6s ease;"></path></svg></div><div class="el-progress__text" style="font-size: 16px;color: #409EFF">${file.percent}%</div></div>
@@ -209,7 +206,6 @@
               let url = JSON.parse(info);
               let sourceLink = domain + "/" + url.key;
 
-
               _this.$http.post(globalConfig.server_user + 'files', {
                 url: sourceLink,
                 name: url.key,
@@ -219,7 +215,6 @@
               }).then((res) => {
                 if (res.data.status === "success") {
                   _this.imgId.push(res.data.data.id);
-
                   let object = {};
                   object.id = res.data.data.id;
                   object.name = res.data.data.name;
@@ -235,7 +230,7 @@
             'Error': function (up, err, errTip) {
 //              console.log(errTip);
             },
-            'UploadComplete': function () {
+            'UploadComplete': function (uploader,files) {
               //队列文件处理完毕后，处理相关的事情
               _this.isUploading = false;
               _this.$notify.success({
