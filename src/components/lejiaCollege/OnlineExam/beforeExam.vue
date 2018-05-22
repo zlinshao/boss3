342c60c3e79d07692c214b40390ae649b7165781
cj_jing <1429407967@qq.com>
2018-05-22 17:39:02 +0800

    'exam'

--- src/components/lejiaCollege/OnlineExam/beforeExam.vue
+++ src/components/lejiaCollege/OnlineExam/beforeExam.vue
@@ -5,19 +5,18 @@
         <div class="title">您目前没有考试</div>
         <div class="content">
           <div class="content_title">最近一场考试</div>
-          <p>场次名称：关于个人信息的问卷</p>
-          <p style="margin-left: 25px;">考试时间：2018-05-03 14：50：00</p>
-
+          <p>场次名称：{{examData.name}}</p>
+          <p>考试时间：{{examData.start_time}}</p>
         </div>
         <p class="view_history" @click="goHistory('exam')">查看历史考试>></p>
       </div>
       <div v-show="showType==='second'">
         <div class="title">开考倒计时</div>
-        <div class="count_down">09:59:59</div>
+        <div class="count_down">{{timeString}}</div>
         <div class="content">
-          <p>场次名称：关于个人信息的问卷</p>
-          <p style="margin-left: 25px;">考试时间：2018-05-03 14：50：00</p>
-          <p style="margin-left: -80px;">试卷时长：60分钟</p>
+          <p>场次名称：{{examData.name}}</p>
+          <p>考试时间：{{examData.start_time}}</p>
+          <p>试卷时长：{{examData.duration}}分钟</p>
         </div>
         <p class="view_history" @click="goHistory('exam')">查看历史考试>></p>
       </div>
@@ -27,13 +26,9 @@
         <div class="content">
           <p>如有疑问，请联系主考官</p>
         </div>
-        <p class="view_history" @click="goHistory('exam')">查看历史考试>></p>
+        <p class="view_history" @click="goHistory">查看历史考试>></p>
       </div>
-
     </div>
-    <el-button @click="showType='first'">first</el-button>
-    <el-button @click="showType='second'">second</el-button>
-    <el-button @click="showType='third'">third</el-button>
   </div>
 
 </template>
@@ -44,16 +39,84 @@
     data() {
       return {
         showType: 'third',
+        examId: '',
+        examData: {},
+        countDown: 0,
+        timeString: '09:59:59',
+        timeClear: '',
       };
     },
-    methods: {
-      goHistory(val){
-        if(val==='exam'){
-          this.$router.push({path: '/LineCollege'});
-        }else{
-          this.$router.push({path: '/LineCollege'});
+    activated() {
+      this.examId = '';
+      this.getExamNaireRedCircle();
+      clearTimeout(this.timeClear);
+    },
+    watch: {
+      examId(val) {
+        if(val){
+          this.getExamData();
+        }
+      },
+      countDown(num) {
+        clearTimeout(this.timeClear);
+        if (num >= 0) {
+          this.clock(num/1000);
         }
       }
+    },
+    methods: {
+      goHistory(val) {
+        this.$router.push({path: '/LineCollege'});
+      },
+      getExamNaireRedCircle() {
+        this.$http.get(globalConfig.server + 'exam/active').then((res) => {
+          if (res.data.code === '30000') {
+            this.examId = res.data.data && res.data.data.id;
+          }
+        });
+      },
+      getExamData() {
+        this.$http.get(globalConfig.server+ 'exam/'+this.examId).then((res)=>{
+          if(res.data.code === '30000') {
+            this.examData = res.data.data;
+            // if((new Date(this.examData.start_time).getTime() - (new Date().getTime())) > 10*60*1000){
+            //   this.showType = 'first';
+            // }else{
+              this.showType = 'second';
+              this.countDown = new Date(res.data.data.start_time).getTime() - (new Date().getTime());
+            // }
+          }
+        });
+      },
+      clock(n) {
+        let val = Number(n);
+        if(val<0){
+          this.$router.push({path: '/answerExam', query: {id: this.examData.id}});
+          return;
+        }
+        let h = Number(Math.floor(val / 3600));
+        let m = 0, s = 0;
+        if ((val - h * 3600) >= 0) {
+          m = Number(Math.floor((val - h * 3600) / 60));
+          if ((val - h * 3600 - m * 60) >= 0) {
+            s = Number(Math.floor(val - h * 3600 - m * 60));
+          }
+        }
+        if (h < 10) {
+          h = '0' + h;
+        }
+        if (m < 10) {
+          m = '0' + m;
+        }
+        if (s < 10) {
+          s = '0' + s;
+        }
+        this.timeString = h + ':' + m + ':' + s;
+        val--;
+        this.timeClear = setTimeout(() => {
+          this.clock(val);
+        }, 1000);
+      },
     }
   };
 </script>
@@ -91,9 +154,9 @@
       display: inline-block;
       padding: 20px 100px;
       position: relative;
+      text-align: left;
       .content_title {
         margin-top: 10px;
-        margin-left: -110px;
       }
     }
     .view_history {
