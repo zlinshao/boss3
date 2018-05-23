44caf98b40a4e19861cb62b3d701f3326dd24045
aigle <840207473@qq.com>
2018-05-23 19:16:47 +0800

    ddd

--- src/components/rentManage/rentingDetail.vue
+++ src/components/rentManage/rentingDetail.vue
@@ -19,7 +19,7 @@
     <div class="container">
       <div class="top">
         <h3>
-          {{contractInfo.community_name}}  {{contractInfo.building}}-{{contractInfo.unit}}-{{contractInfo.doorplate}}
+          {{contractInfo.community_name}}  {{contractInfo.doorplate_str}}
           <span v-if="contractInfo.contract_number">（合同编号 : {{contractInfo.contract_number}}）</span>
         </h3>
         <h3>
