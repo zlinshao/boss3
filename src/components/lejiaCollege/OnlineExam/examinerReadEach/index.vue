6175766b5e471ae779f39edabc85eed8737f7400
cj_jing <1429407967@qq.com>
2018-05-25 11:27:35 +0800

    'undefined'

--- src/components/lejiaCollege/OnlineExam/examinerReadEach/index.vue
+++ src/components/lejiaCollege/OnlineExam/examinerReadEach/index.vue
@@ -5,9 +5,9 @@
         <div class="highSearch">
           <el-form :inline="true" onsubmit="return false" size="mini">
             <el-form-item>
-              <el-input placeholder="标题/内容关键字" v-model="form.search" @keyup.enter.native="myData()" size="mini"
+              <el-input placeholder="请输入" v-model="form.search" @keyup.enter.native="search" size="mini"
                         clearable>
-                <el-button slot="append" icon="el-icon-search" @click="myData()"></el-button>
+                <el-button slot="append" icon="el-icon-search" @click="search"></el-button>
                 <!--<el-button slot="append" icon="el-icons-fa-bars"></el-button>-->
               </el-input>
             </el-form-item>
@@ -25,72 +25,23 @@
               <el-col :span="12">
                 <el-row>
                   <el-col :span="8">
-                    <div class="el_col_label">选择类型</div>
-                  </el-col>
-                  <el-col :span="16" class="el_col_option">
-                    <el-form-item>
-                      <el-select v-model="form.type" clearable placeholder="请选择类型">
-                        <el-option v-for="(key,index) in forms" :label="key.name" :value="key.id"
-                                   :key="index"></el-option>
-                      </el-select>
-                    </el-form-item>
-                  </el-col>
-                </el-row>
-              </el-col>
-              <el-col :span="12">
-                <el-row>
-                  <el-col :span="8">
-                    <div class="el_col_label">时间</div>
-                  </el-col>
-                  <el-col :span="16" class="el_col_option">
-                    <el-date-picker
-                      size="mini"
-                      v-model="value4"
-                      type="datetimerange"
-                      :picker-options="pickerOptions2"
-                      value-format="yyyy-MM-dd"
-                      format="yyyy-MM-dd"
-                      range-separator="至"
-                      start-placeholder="开始日期"
-                      end-placeholder="结束日期"
-                      align="right">
-                    </el-date-picker>
-                  </el-col>
-                </el-row>
-              </el-col>
-            </el-row>
-            <el-row class="el_row_border">
-              <el-col :span="12">
-                <el-row>
-                  <el-col :span="8">
-                    <div class="el_col_label">选择试卷</div>
-                  </el-col>
-                  <el-col :span="16" class="el_col_option">
-                    <el-form-item>
-                      <el-select v-model="form.type" clearable placeholder="请选择试卷">
-                        <el-option v-for="(key,index) in forms" :label="key.name" :value="key.id"
-                                   :key="index"></el-option>
-                      </el-select>
-                    </el-form-item>
-                  </el-col>
-                </el-row>
-              </el-col>
-              <el-col :span="12">
-                <el-row>
-                  <el-col :span="8">
                     <div class="el_col_label">部门搜索</div>
                   </el-col>
                   <el-col :span="16" class="el_col_option">
                     <el-form-item>
-                      <el-input readonly="" v-model="departname" @click.native="openOrganizationModal()"
-                                placeholder="点击选择"></el-input>
+                      <el-input readonly="" v-model="departname" @focus="openOrganizationModal"
+                                placeholder="点击选择">
+                        <template slot="append">
+                          <div style="cursor: pointer;" @click="emptyDepart">清空</div>
+                        </template>
+                      </el-input>
                     </el-form-item>
                   </el-col>
                 </el-row>
               </el-col>
             </el-row>
             <div class="btnOperate">
-              <el-button size="mini" type="primary" @click="myData()">搜索</el-button>
+              <el-button size="mini" type="primary" @click="search">搜索</el-button>
               <el-button size="mini" type="primary" @click="resetting">重置</el-button>
               <el-button size="mini" type="primary" @click="highGrade">取消</el-button>
             </div>
@@ -107,7 +58,6 @@
               element-loading-text="拼命加载中"
               element-loading-spinner="el-icon-loading"
               element-loading-background="rgba(255, 255, 255, 0)"
-              @row-dblclick="dblClickTable"
               style="width: 100%">
               <el-table-column
                 prop="start_time"
@@ -148,7 +98,9 @@
               <el-table-column
                 label="操作">
                 <template slot-scope="scope">
-                  <span v-if="scope.row.result_id == 0 || (scope.row.result_info && scope.row.result_info.waiting && scope.row.result_info.waiting.length == 0)" style="cursor: pointer;color: #6a8dfb;" @click="lookExam(scope.row)">已完成,点击查看</span>
+                  <span
+                    v-if="scope.row.result_id == 0 || (scope.row.result_info && scope.row.result_info.waiting && scope.row.result_info.waiting.length == 0)"
+                    style="cursor: pointer;color: #6a8dfb;" @click="lookExam(scope.row)">已完成,点击查看</span>
                   <span v-else @click="correctExam(scope.row)" style="cursor: pointer;color: #6a8dfb;">点击阅卷</span>
                 </template>
               </el-table-column>
@@ -167,8 +119,8 @@
         </div>
       </div>
     </div>
-    <Organization :organizationDialog="organizationDialog" :length="len" :type="depart" @close="closeOrganization"
-                  @selectMember="coloseaa"></Organization>
+    <Organization :organizationDialog="organizationDialog"  :type="organizeType" @close="closeOrganization"
+                  @selectMember="selectMember"></Organization>
   </div>
 </template>
 
@@ -176,7 +128,7 @@
   import Organization from "../../../common/organization.vue";
 
   export default {
-    name: "index",
+    name: "examiner-read-each",
     components: {Organization},
     data() {
       return {
@@ -186,24 +138,15 @@
         form: {
           page: 1,
           limit: 12,
-          type: "",
-          search: "",
-          department_id: ""
+          department_id: ''
         },
-        departname: "",
-        forms: [
-          {id: "1", name: "表彰"},
-          {id: "2", name: "批评"},
-          {id: "3", name: "通知"}
-          // { id: "4", name: "研发" }
-        ],
+        departname: '',
         isHigh: false, //高级搜索
-        rentStatus: " ",
-        rentLoading: false,
+        organizeType: '',
         organizationDialog: false,
-        rentStatus: " ",
+        rentStatus: ' ',
         rentLoading: false,
-        pickerOptions2: {
+        pickerOptions: {
           shortcuts: [
             {
               text: "最近一周",
@@ -234,9 +177,6 @@
             }
           ]
         },
-        value4: "",
-        len: 0,
-        depart: ""
       };
     },
     activated() {
@@ -245,10 +185,14 @@
     },
     watch: {},
     methods: {
+      search(){
+        this.form.page = 1;
+        this.myData();
+      },
       correctExam(val) {
         this.$router.push({path: "/examinerShortAn", query: {result_id: val.result_id, exam_id: val.exam_id}});
       },
-      lookExam(val){
+      lookExam(val) {
         this.$router.push({path: '/lookExam', query: {result_id: val.result_id, exam_id: val.exam_id, from: 'manage'}});
       },
       getQueryData() {
@@ -263,8 +207,9 @@
       myData() {
         this.rentStatus = " ";
         this.rentLoading = true;
-        this.$http.get(globalConfig.server + "exam/finished/" + this.examId).then(res => {
+        this.$http.get(globalConfig.server + "exam/finished/" + this.examId, {params: this.form}).then(res => {
           this.rentLoading = false;
+          this.isHigh = false;
           if (res.data.code == "30000") {
             this.tableData = res.data.data.data;
             this.tableNumber = res.data.data.count;
@@ -277,19 +222,20 @@
       },
       openOrganizationModal() {
         this.organizationDialog = true;
-        this.len = 1;
-        this.depart = "depart";
+        this.organizeType = 'depart';
       },
       closeOrganization() {
         this.organizationDialog = false;
-        this.len = 0;
-        this.depart = "";
+        this.organizeType = '';
       },
-      coloseaa(val) {
+      selectMember(val) {
         this.departname = val[0].name;
         this.form.department_id = val[0].id;
-        this.len = 0;
-        this.depart = "";
+        this.organizeType = '';
+      },
+      emptyDepart() {
+        this.departname = '';
+        this.form.department_id = '';
       },
       // 高级
       highGrade() {
@@ -297,15 +243,13 @@
       },
       // 重置
       resetting() {
-        this.isHigh = false;
         this.form = {
           page: 1,
           limit: 12,
-          type: "",
-          search: ""
+          search: '',
+          department_id: ''
         };
-      },
-      dblClickTable() {
+        this.departname = '';
       },
       handleSizeChange(val) {
         console.log(`每页 ${val} 条`);
