5b97edd549377d0c9689bfad373de97eadfc6d54
cj_jing <1429407967@qq.com>
2018-05-28 11:03:21 +0800

    'subjective_detail'

--- src/components/lejiaCollege/OnlineExam/lookExam/index.vue
+++ src/components/lejiaCollege/OnlineExam/lookExam/index.vue
@@ -185,7 +185,7 @@
               </el-col>
             </el-row>
             <div style="line-height: 30px;font-size: 15px;">
-              <span style="color:#409EFF;margin-left: 50px;" >本题得分： <span v-if="resultData.objective_detail">{{resultData.objective_detail[item.id]}}</span><span v-else>暂无</span></span>
+              <span style="color:#409EFF;margin-left: 50px;" >本题得分： <span v-if="resultData.subjective_detail">{{resultData.subjective_detail[item.id]}}</span><span v-else>暂无</span></span>
             </div>
             <el-form>
               <el-form-item style="width: 96%;margin-left: 50px;margin-top: 10px;">
