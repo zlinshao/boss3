0844700f4ac8f10bc0908f217288ea3cba33d378
cj_jing <1429407967@qq.com>
2018-05-31 13:17:49 +0800

    'noPower'

--- src/components/systemSetting/moduleSetting/replierManage/index.vue
+++ src/components/systemSetting/moduleSetting/replierManage/index.vue
@@ -4,17 +4,19 @@
       <div class="highRanking" style=" position: absolute; top: 120px; right: 20px;">
         <div class="highSearch">
           <el-form :inline="true" onsubmit="return false" size="medium">
-            <el-form-item>
-              <el-input placeholder="标题/内容关键字" v-model="form.search" @keyup.enter.native="search" size="mini"
-                        clearable>
-                <el-button slot="append" icon="el-icon-search" @click="search"></el-button>
-              </el-input>
-            </el-form-item>
+            <!--<el-form-item>-->
+            <!--<el-input placeholder="标题/内容关键字" v-model="form.search" @keyup.enter.native="search" size="mini"-->
+            <!--clearable>-->
+            <!--<el-button slot="append" icon="el-icon-search" @click="search"></el-button>-->
+            <!--</el-input>-->
+            <!--</el-form-item>-->
             <el-form-item>
               <el-button type="primary" size="mini" @click="highGrade">高级</el-button>
             </el-form-item>
             <el-form-item>
-              <el-button type="primary" size="mini" @click="openOrganizationModal('staff')">分发问答</el-button>
+              <el-button type="primary" size="mini" @click="openReplierDialog">
+                <i class="el-icon-plus"></i>新增
+              </el-button>
             </el-form-item>
           </el-form>
         </div>
@@ -33,10 +35,10 @@
                   </el-col>
                   <el-col :span="16" class="el_col_option">
                     <el-form-item>
-                      <el-input readonly="" v-model="departname" @focus="openOrganizationModal('depart')"
+                      <el-input readonly="" v-model="form.department_name" @focus="chooseDepart('search')"
                                 placeholder="点击选择">
                         <template slot="append">
-                          <div style="cursor: pointer;" @click="emptyDepart">清空</div>
+                          <div style="cursor: pointer;" @click="emptyDepart('search')">清空</div>
                         </template>
                       </el-input>
                     </el-form-item>
@@ -63,39 +65,14 @@
               element-loading-text="拼命加载中"
               element-loading-spinner="el-icon-loading"
               element-loading-background="rgba(255, 255, 255, 0)"
-              @selection-change="handleSelectionChange"
               style="width: 100%">
               <el-table-column
-                type="selection"
-                width="65">
-              </el-table-column>
-              <el-table-column
-                prop="title"
-                label="标题">
-              </el-table-column>
-              <el-table-column
-                prop="create_time"
-                label="创建时间">
-              </el-table-column>
-              <el-table-column
-                prop="asker"
-                label="提问者">
-              </el-table-column>
-              <el-table-column
-                prop="description"
-                label="描述">
-              </el-table-column>
-              <el-table-column
-                prop=""
-                label="职位">
-              </el-table-column>
-              <el-table-column
-                prop=""
+                prop="department.name"
                 label="部门">
               </el-table-column>
               <el-table-column
-                prop=""
-                label="所属部门">
+                prop="replier_id"
+                label="回复人">
               </el-table-column>
             </el-table>
           </div>
@@ -116,6 +93,38 @@
     </div>
     <Organization :organizationDialog="organizationDialog" :type="organizeType" @close="closeOrganization"
                   @selectMember="selectMember"></Organization>
+    <div id="replierDialog">
+      <el-dialog :close-on-click-modal="false" :visible.sync="replierDialog" title="新增回复" width="30%">
+        <el-form size="mini" onsubmit="return false;" :model="replierForm" label-width="100px">
+          <el-row>
+            <el-col :span="20">
+              <el-form-item label="部门" required>
+                <el-input v-model="replierForm.department_name" placeholder="请点击选择" @focus="chooseDepart('dialog')">
+                  <template slot="append">
+                    <div style="cursor: pointer;" @click="emptyDepart('dialog')">清空</div>
+                  </template>
+                </el-input>
+              </el-form-item>
+            </el-col>
+          </el-row>
+          <el-row>
+            <el-col :span="20">
+              <el-form-item label="回复人" required>
+                <el-input v-model="replierForm.replier_name" placeholder="请点击选择" @focus="chooseStaff">
+                  <template slot="append">
+                    <div style="cursor: pointer;" @click="emptyStaff">清空</div>
+                  </template>
+                </el-input>
+              </el-form-item>
+            </el-col>
+          </el-row>
+        </el-form>
+        <span slot="footer" class="dialog-footer">
+          <el-button size="small" @click="replierDialog = false;replierId = '';">取消</el-button>
+          <el-button size="small" type="primary" @click="confirmAdd">保存</el-button>
+        </span>
+      </el-dialog>
+    </div>
   </div>
 </template>
 <script>
@@ -126,55 +135,129 @@
     data() {
       return {
         tableData: [],
+        totalNum: 0,
+        tableStatus: ' ',
+        tableLoading: false,
         form: {
           page: 1,
           limit: 12,
-          search: '',
           department_id: '',
+          department_name: '',
         },
-        totalNum: 0,
+        replierForm: {
+          department_id: '',  //部门
+          replier_id: [],  //回复人
+          department_name: '',
+          replier_name: '',
+        },
         isHigh: false,
-        tableStatus: ' ',
-        tableLoading: false,
-        departname: '',
         organizationDialog: false,
         organizeType: '',
-        askAnswerIds: [],  //问答编号
-        userIds: '',
+        replierDialog: false,
+        replierId: '',
+        currentStatus: '',
       };
     },
     mounted() {
       this.getTableData();
     },
+    watch: {
+      replierDialog(val) {
+        if (val) {
+          this.replierForm = {
+            department_id: '',  //部门
+            replier_id: [],  //回复人
+            department_name: '',
+            replier_name: '',
+          };
+        }
+      },
+    },
     methods: {
-      handleSelectionChange(val) {
-        this.askAnswerIds = [];
-        val.forEach((item) => {
-          this.askAnswerIds.push(item.id);
-        });
+      chooseDepart(val) {
+        this.organizationDialog = true;
+        this.organizeType = 'depart';
+        this.currentStatus = val;
+      },
+      chooseStaff() {
+        this.organizationDialog = true;
+        this.organizeType = 'staff';
+        this.currentStatus = 'dialog';
+      },
+      closeOrganization() {
+        this.organizationDialog = false;
+        this.organizeType = '';
+        this.currentStatus = '';
+      },
+      selectMember(val) {
+        if (this.organizeType === 'staff') {
+          let names = [];
+          val.forEach((item) => {
+            this.replierForm.replier_id.push(item.id);
+            names.push(item.name);
+          });
+          this.replierForm.replier_name = names.join(',');
+        } else {
+          if (this.currentStatus === 'search') {
+            this.form.department_id = val[0].id;
+            this.form.department_name = val[0].name;
+          } else {
+            this.replierForm.department_id = val[0].id;
+            this.replierForm.department_name = val[0].name;
+          }
+        }
+        this.organizeType = '';
+      },
+      emptyStaff() {
+        this.replierForm.replier_id = [];
+        this.replierForm.replier_name = '';
+      },
+      emptyDepart(val) {
+        if (val === 'search') {
+          this.form.department_name = '';
+          this.form.department_id = '';
+        } else {
+          this.replierForm.department_name = '';
+          this.replierForm.department_id = '';
+        }
       },
       search() {
         this.form.page = 1;
         this.getTableData();
+      },
+      openReplierDialog() {
+        this.replierDialog = true;
+        this.replierForm = {
+          department_id: '',
+          replier_id: [],
+          department_name: '',
+          replier_name: '',
+        };
+      },
+      confirmAdd() {
+        this.$http.post(globalConfig.server + 'qa/replier_manage', this.replierForm).then((res) => {
+          if (res.data.code === '70010') {
+            this.replierDialog = false;
+            this.$notify.success({
+              title: '成功',
+              message: res.data.msg
+            });
+            this.getTableData();
+          }
+        });
       },
       getTableData() {
         this.tableStatus = ' ';
         this.tableLoading = true;
         this.$http.get(globalConfig.server + 'qa/replier_manage', {params: this.form}).then((res) => {
           this.tableLoading = false;
-          if (res.data.code === '199200') {
+          if (res.data.code === '70010') {
             this.tableData = res.data.data;
-            this.tableNum = res.data.count;  //记录总条数
+            this.totalNum = res.data.meta.num;  //记录总条数
           } else {
             this.tableStatus = '暂无数据';
-            this.tableNum = 0;
+            this.totalNum = 0;
           }
-        });
-      },
-      //分发部门
-      distribute() {
-        this.$http.post(globalConfig.server + 'ans/send',{id: this.askAnswerIds, uids: this.userIds}).then((res) => {
-
         });
       },
       handleSizeChange(val) {
@@ -184,32 +267,6 @@
         console.log(`当前页: ${val}`);
         this.form.page = val;
         this.getTableData();
-      },
-      openOrganizationModal(val) {
-        this.organizationDialog = true;
-        if(val === 'staff'){
-          this.organizeType = 'staff';
-        }else{
-          this.organizeType = 'depart';
-        }
-      },
-      closeOrganization() {
-        this.organizationDialog = false;
-        this.organizeType = '';
-      },
-      selectMember(val) {
-        if(this.organizeType === 'staff'){
-          this.userIds = val[0].id;
-          this.distribute();
-        }else{
-          this.departname = val[0].name;
-          this.form.department_id = val[0].id;
-        }
-        this.organizeType = '';
-      },
-      emptyDepart() {
-        this.departname = '';
-        this.form.department_id = '';
       },
       // 高级
       highGrade() {
@@ -222,8 +279,8 @@
           limit: 12,
           search: '',
           department_id: '',
+          department_name: '',
         };
-        this.departname = '';
       },
     },
   };
