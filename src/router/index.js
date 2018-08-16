9d6b59c1ac9194142b0b9ce30047fe06020ddf2e
aigle <840207473@qq.com>
2018-08-16 17:25:08 +0800

    ddd

--- src/router/index.js
+++ src/router/index.js
@@ -594,16 +594,16 @@ export default new Router({
         // {path: '/exportManage', component: ExportManage, name: '导出管理',},
       ]
     },
-    {
-      path: '/',
-      component: Index,
-      name: 'C端管理',
-      icon: 'iconfont icon-shezhi1',
-      children: [
-        {path: '/application', component: Application, name: '申请管理'},
-        {path: '/houses', component: Houses, name: '房源管理'},
-      ]
-    },
+    // {
+    //   path: '/',
+    //   component: Index,
+    //   name: 'C端管理',
+    //   icon: 'iconfont icon-shezhi1',
+    //   children: [
+    //     {path: '/application', component: Application, name: '申请管理'},
+    //     {path: '/houses', component: Houses, name: '房源管理'},
+    //   ]
+    // },
   ]
 })
 
