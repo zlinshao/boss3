a7aa375a05a3510a3372aea54b85f15e1d54d2ba
lc1246877006 <lichao_lc123@126.com>
2018-11-12 14:43:46 +0800

    'xiugaixiaoqu'

---
+++ src/components/lejiaCollege/VideoTeaching/videoDetails.vue
@@ -0,0 +1,113 @@
+<template>
+  <div id="videoList">
+    <div class="videoBtn">
+      <!-- <el-button icon="el-icon-picture-outline" type="primary" size="mini" >上传视屏</el-button>  -->
+      <el-button icon="el-icon-picture-outline" type="primary" size="mini" @click="createAlbum = true">视屏分类</el-button>
+    </div>
+    <div class="videoClassification">
+      <!-- 相册列表 -->
+      <el-row>
+        <el-col :span="3" v-for="(o, index) in 2" :key="o" :offset="index" style="margin-bottom: 20px">
+          <el-card :body-style="{ padding: '0px', height: '255px'}">
+            <img src="../../../assets/images/university/caia412-34427.png" class="image">
+            <div style="padding: 14px;">
+              <span>好吃的汉堡</span>
+              <div class="bottom clearfix">
+                <!-- <time class="time">{{ currentDate }}</time> -->
+                <el-tooltip content="删除" placement="bottom" effect="light">
+                  <el-button type="text" class="button" >
+                    <i class="el-icon-delete"></i>
+                  </el-button>
+                </el-tooltip>
+                <el-tooltip content="编辑" placement="bottom" effect="light">
+                  <el-button type="text" class="button">
+                    <i class="el-icon-edit"></i>
+                  </el-button>
+                </el-tooltip>
+              </div>
+            </div>
+          </el-card>
+        </el-col>
+        <!-- 创建视屏相册 -->
+        <el-dialog title="提示" :visible.sync="createAlbum" width="30%" class="createdVideo">
+          <el-form>
+            <el-row>
+              <el-form-item label="视屏册名称:" required>
+                <el-input v-model="videoForm.name" placeholder="请输入视屏册名称"></el-input>
+              </el-form-item>
+            </el-row>
+            <el-row>
+              <el-form-item label="视屏册说明:">
+                <el-input v-model="videoForm.description" type="textarea" placeholder="请输入视屏册说明"></el-input>
+              </el-form-item>
+            </el-row>
+          </el-form>
+          <span slot="footer" class="dialog-footer">
+            <el-button @click="createAlbum = false" size="mini">取 消</el-button>
+            <el-button type="primary" @click="createVideo" size="mini">确 定</el-button>
+          </span>
+        </el-dialog>
+      </el-row>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      createAlbum: false,  //创建视屏册
+      videoForm: {    //
+         name: '',
+        description: '',
+      }
+    };
+  },
+  methods: {
+    // 创建视屏相册
+    createVideo() {
+      this.createAlbum = false;
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+#videoList {
+  .videoBtn {
+    margin-left: 17px;
+    margin-top: 15px;
+    color: #6a8dfb;
+    padding-top: 10px;
+    padding-bottom: 20px;
+    border-bottom: 1px solid #e5e5e5;
+  }
+  .videoClassification {
+    margin: 17px;
+    .image {
+      width: 100%;
+      height: 180px;
+      display: block;
+      cursor: pointer;
+    }
+    .bottom {
+      margin-top: 13px;
+      line-height: 12px;
+    }
+    .button {
+      padding: 0;
+      margin-left: 5px;
+      float: right;
+    }
+  }
+  .createdVideo {
+    .el-input,
+    .el-textarea {
+      width: 80%;
+    }
+    .el-textarea {
+      // margin-left: 10px;
+    }
+  }
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -111,7 +111,8 @@ const LineCollege = () => import  ('../components/lejiaCollege/LineCollege/index
 const StaffRegistration = () => import  ('../components/lejiaCollege/StaffRegistration/index.vue');
 const College = () => import  ('../components/lejiaCollege/college/index.vue'); //乐伽大学
 const OnlineExam = () => import  ('../components/lejiaCollege/OnlineExam/index.vue');    //在线考试
-const VideoTeaching = () => import('../components/lejiaCollege/VideoTeaching/index.vue')  //教学视频
+const VideoTeaching = () => import('../components/lejiaCollege/VideoTeaching/index.vue')  //教学视频详情
+const videoDetails = () => import('../components/lejiaCollege/VideoTeaching/videoDetails.vue') //教学视频管理
 
 const ConfigExam = () => import  ('../components/lejiaCollege/OnlineExam/configExam/index.vue');  //管理试卷
 const BatchQuestions = () => import  ('../components/lejiaCollege/OnlineExam/batchQuestions/index.vue');  //批量导入试题
@@ -639,7 +640,7 @@ export default new Router({
         {path: '/college', component: College, name: '乐伽大学 '},
         {path: '/OnlineExam', component: OnlineExam, name: '在线考试'},
         {path: '/examinerRead', component: ExaminerRead, name: '考官判卷'},
-        // { path: '/videoTeaching', component: VideoTeaching, name: '教学视频'},
+        // { path: '/videoDetails', component: videoDetails, name: '教学视频'},
         
       ]
     },
