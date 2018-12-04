cd00b50b652c35e91d5339073b24ddbde796d4df
shuisheng <njshuisheng@126.com>
2018-12-04 19:04:36 +0800

    feat: approval

--- src/components/approval/components/approvalDetail.vue
+++ src/components/approval/components/approvalDetail.vue
@@ -5,7 +5,21 @@
         :visible.sync="innerVisible"
         @close="handleClose"
       >
-        this is approvalVisible
+        <div class="container">
+            <div class="detail" v-for="(item,key) in detail">
+              <div v-if="key !== '附件上传'">
+                {{key}} : <div class="content">{{item ? item : '无'}}</div>
+              </div>
+              <div v-else>
+                {{key}} :
+                <div v-if="detail[key]['data']" class="content">
+                <img v-for="value in detail['附件上传'].data" :src=`${imgUrl}${value['attachment_path']}?api_token=${key['token']}`
+                data-magnify="" :data-src=`${imgUrl}${value['attachment_path']}?api_token=${key['token']}`>
+                </div>
+                <div v-else class="content">暂无</div>
+              </div>
+            </div>
+        </div>
       </el-dialog>
     </div>
 </template>
@@ -15,14 +29,23 @@
       name: "approvalDetail",
       data() {
         return {
-          innerVisible: false
+          innerVisible: false,
+          detailData: '',
+          imgUrl: "http://oa.lejias.cn/eoffice10/server/public/api/",
         }
       },
-      props: ['approvalVisible'],
+      props: ['approvalVisible','detail'],
       watch:{
         approvalVisible:function (val) {
           this.innerVisible = val;
+        },
+        detail: function (val) {
+          this.detailData = val;
+          console.log(this.detailData);
         }
+      },
+      mounted() {
+        console.log(this.detail);
       },
       methods: {
         handleClose() {
@@ -33,5 +56,25 @@
 </script>
 
 <style scoped lang="scss">
-
+  #approvalDetail{
+    .container{
+      width: 100%;
+      display: flex;
+      justify-content: space-between;
+      flex-wrap: wrap;
+    }
+    .detail{
+      width: 30%;
+      line-height: 30px;
+      text-align: left;
+      .content{
+        padding: 0 10px;
+        min-height: 32px;
+        background: #eef3fc;
+        border-radius: 4px;
+        font-size: 12px;
+        color: #727479;
+      }
+    }
+  }
 </style>
