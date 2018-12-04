ea5723256d997449c28dee88c9b031f775fa506f
shuisheng <njshuisheng@126.com>
2018-12-04 11:23:46 +0800

    feat: approval

---
+++ src/components/approval/components/approvalDetail.vue
@@ -0,0 +1,37 @@
+<template>
+    <div id="approvalDetail">
+      <el-dialog
+        title="审批详情"
+        :visible.sync="innerVisible"
+        @close="handleClose"
+      >
+        this is approvalVisible
+      </el-dialog>
+    </div>
+</template>
+
+<script>
+    export default {
+      name: "approvalDetail",
+      data() {
+        return {
+          innerVisible: false
+        }
+      },
+      props: ['approvalVisible'],
+      watch:{
+        approvalVisible:function (val) {
+          this.innerVisible = val;
+        }
+      },
+      methods: {
+        handleClose() {
+          this.$emit('approvalClose');
+        }
+      }
+    }
+</script>
+
+<style scoped lang="scss">
+
+</style>
---
+++ src/components/approval/index.vue
@@ -0,0 +1,46 @@
+<template>
+    <div id="approval">
+      <el-table
+        :data="tableList"
+        @row-click="LookDetail"
+      >
+        <el-table-column prop="name" label="名称"></el-table-column>
+        <el-table-column prop="date" label="时间"></el-table-column>
+        <el-table-column prop="approvalPeople" label="审批人"></el-table-column>
+      </el-table>
+
+      <ApprovalPeople :approvalVisible="approvalVisible" @approvalClose="handleCloseApproval"></ApprovalPeople>
+    </div>
+</template>
+
+<script>
+  import ApprovalPeople from './components/approvalDetail';
+    export default {
+      name: "index",
+      components: { ApprovalPeople },
+      data() {
+        return {
+          tableList: [
+            {name: '请假审批',date:'2018-12-01',approvalPeople:'HS'},
+            {name: '事假审批',date:'2018-12-02',approvalPeople:'HS'},
+            {name: '请假审批',date:'2018-12-03',approvalPeople:'HS'},
+            {name: '婚假审批',date:'2018-12-04',approvalPeople:'HS'}
+          ],
+          approvalVisible: false
+        }
+      },
+      methods: {
+        LookDetail(row) {
+          console.log(row);
+          this.approvalVisible = true;
+        },
+        handleCloseApproval() {
+          this.approvalVisible = false;
+        },
+      }
+    }
+</script>
+
+<style scoped lang="scss">
+
+</style>
