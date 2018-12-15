861a18247498dfa9fcc7f2f67d6f6590d2e677d7
guomin <yushi5344@outlook.com>
2018-12-15 16:23:38 +0800

    financeNew

---
+++ src/components/financeNew/accountPayable/index.vue
@@ -0,0 +1,25 @@
+<template >
+   <div id="accountPayable">
+       应付款项
+   </div>
+</template>
+<script>
+    export default{
+        name:'accountPayable',
+        data(){
+            return{
+                
+            }
+        },
+        mounted(){
+
+        },
+        methods:{
+
+        }
+    }
+
+</script>
+<style scoped>
+
+</style>
