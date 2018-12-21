8afcfb412367090bbad509be3f70aeb03cc46e24
shuisheng <njshuisheng@126.com>
2018-12-21 11:48:45 +0800

    feat: finance new

---
+++ src/components/financeNew/accountManage/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="accountManage">
+      this is account manage
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "index.vue"
+    }
+</script>
+
+<style scoped>
+
+</style>
