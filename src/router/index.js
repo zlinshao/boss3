d7812228727806ce2bf594ba61fadca0079dd580
linshao <601267015@qq.com>
2019-01-04 11:11:03 +0800

    fff

--- src/router/index.js
+++ src/router/index.js
@@ -577,24 +577,24 @@ export default new Router({
         {path: '/approval',component: Approval, name: '审批管理'}
       ]
     },
-    // {
-    //   path: '/',
-    //   component: Index,
-    //   name: 'HRM',
-    //   icon: 'iconfont icon-renzi',
-    //   children: [
-    //     {
-    //       path: '/teamManage', component: TeamManage, name: '人员信息管理',
-    //       children: [
-    //         {path: '/teamManage/roster', component: Roster, name: '花名册'},
-    //         // {path: '/teamManage/entryManage', component: EntryManage, name: '入职管理'},
-    //         {path: '/becomeFormal', component: BecomeFormal, name: '转正管理'},
-    //         {path: '/transferPosition', component: TransferPosition, name: '调岗管理'},
-    //         {path: '/dimission', component: Dimission, name: '离职管理'},
-    //       ],
-    //     },
-    //   ]
-    // },
+    {
+      path: '/',
+      component: Index,
+      name: 'HRM',
+      icon: 'iconfont icon-renzi',
+      children: [
+        {
+          path: '/teamManage', component: TeamManage, name: '人员信息管理',
+          children: [
+            {path: '/teamManage/roster', component: Roster, name: '花名册'},
+            // {path: '/teamManage/entryManage', component: EntryManage, name: '入职管理'},
+            {path: '/becomeFormal', component: BecomeFormal, name: '转正管理'},
+            {path: '/transferPosition', component: TransferPosition, name: '调岗管理'},
+            {path: '/dimission', component: Dimission, name: '离职管理'},
+          ],
+        },
+      ]
+    },
     {
       path: '/staffDetail',
       component: StaffDetail,
