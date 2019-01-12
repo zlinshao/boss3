0de1170eb173176ff3ce34a88e49524d1118f247
shuisheng <njshuisheng@126.com>
2019-01-12 16:25:23 +0800

    fix: finance

--- src/components/financeNew/accountPayable/index.vue
+++ src/components/financeNew/accountPayable/index.vue
@@ -353,7 +353,7 @@
               <span class="receive_detail" v-else>/</span>
             </el-col>
             <el-col :span="6">
-              <span style="color: #409EFF;" class="receive_title">详情：</span>
+              <span style="color: #409EFF;" class="receive_title">备注：</span>
               <span class="receive_detail" v-if="DetailCurrentRow.tags">
                 <span v-for="item in DetailCurrentRow.tags">{{ item.content }};</span>
               </span>
@@ -408,8 +408,8 @@
           </el-row>
           <el-row :gutter="20" style="margin-bottom: 25px">
             <el-col :span="6">
-              <span style="color: #409EFF;" class="receive_title">备注：</span>
-              <span class="receive_detail" v-if="DetailCurrentRow.remark">{{ DetailCurrentRow.remark }}</span>
+              <span style="color: #409EFF;" class="receive_title">详情：</span>
+              <span class="receive_detail" v-if="DetailCurrentRow.description && DetailCurrentRow.description.description">{{ DetailCurrentRow.description.description }}</span>
               <span class="receive_detail" v-else>/</span>
             </el-col>
           </el-row>
