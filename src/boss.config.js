656f309d4dbc89f649a238d0fc35ecee82534470
shuisheng <njshuisheng@126.com>
2019-03-09 18:17:00 +0800

    bank running

--- src/boss.config.js
+++ src/boss.config.js
@@ -1,5 +1,5 @@
 globalConfig = {
-  temporary_server: 'http://zz.cn.vaiwan.com/api/',
+  temporary_server: 'http://test.v3.api.boss.lejias.cn/api/',
   finance_server: 'http://test.api.finance.lejias.cn/',
   server: 'http://test.v3.api.boss.lejias.cn/',                       //接口地址
   server_user :'http://test.boss-support.lejias.cn/api/s1/',          //人资模块接口地址
